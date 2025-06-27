97d8b57926d111190fb6808507b1f8ce81785e69
Christian Hoock <9932295+wk1@users.noreply.github.com>
2025-06-27 17:30:22 +0200

    Update valid input logic

--- script/main_menu.py
+++ script/main_menu.py
@@ -19,103 +19,102 @@ def main():
   input_text, notes = processInput()
 
   items = []
-  add_valid = False
 
-  if input_text:
-    items.append({
-      "title": f"Inbox",
-      "subtitle": f"Create Task \"{input_text}\" in Inbox",
-      "arg": input_text,
-      "variables": {
-        "input_text": input_text,
-        "notes": notes,
-        "action": ActionType.ADD.value,
-        "item_type": ItemType.TASK.value,
-        "container_type": ContainerType.INBOX.value,
-        "container_title": "📥 Inbox",
-      },
-      "icon": {"path": f"icons/{theme}/inbox.png"},
-      "valid": add_valid,
-      "mods": {
-        "cmd": {
-          "variables": {
-            "input_text": input_text,
-            "notes": notes,
-            "action": ActionType.ADD.value,
-            "item_type": ItemType.TASK.value,
-            "container_type": ContainerType.INBOX.value,
-            "container_title": "📥 Inbox",
-            "batch": 1
-          },
-          "subtitle": f"Batch create Task \"{input_text}\" in Inbox"
-        }
-      }
-    })
-
-    items.append({
-      "title": f"Today",
-      "subtitle": f"Create Task \"{input_text}\" in Today",
-      "arg": input_text,
-      "variables": {
-        "input_text": input_text,
-        "notes": notes,
-        "action": ActionType.ADD.value,
-        "item_type": ItemType.TASK.value,
-        "container_type": ContainerType.TODAY.value,
-        "container_title": "📆 Today",
-      },
-      "icon": {"path": f"icons/{theme}/today.png"},
-      "valid": True,
-      "mods": {
-        "option": {
-          "valid": True,
-          "arg": input_text,
-          "variables": {
-            "input_text": input_text,
-            "notes": notes,
-            "action": ActionType.ADD.value,
-            "item_type": ItemType.PROJECT.value,
-            "container_type": ContainerType.TODAY.value,
-            "container_title": "📆 Today",
-          },
-          "subtitle": f"Create Project \"{input_text}\" for Today"
+  # if input_text:
+  items.append({
+    "title": f"Inbox",
+    "subtitle": f"Create Task \"{input_text}\" in Inbox",
+    "arg": input_text,
+    "variables": {
+      "input_text": input_text,
+      "notes": notes,
+      "action": ActionType.ADD.value,
+      "item_type": ItemType.TASK.value,
+      "container_type": ContainerType.INBOX.value,
+      "container_title": "📥 Inbox",
+    },
+    "icon": {"path": f"icons/{theme}/inbox.png"},
+    "valid": bool(input_text),
+    "mods": {
+      "cmd": {
+        "variables": {
+          "input_text": input_text,
+          "notes": notes,
+          "action": ActionType.ADD.value,
+          "item_type": ItemType.TASK.value,
+          "container_type": ContainerType.INBOX.value,
+          "container_title": "📥 Inbox",
+          "batch": 1
         },
-        "cmd": {
-          "valid": True,
-          "arg": input_text,
-          "variables": {
-            "input_text": input_text,
-            "notes": notes,
-            "action": ActionType.ADD.value,
-            "item_type": ItemType.TASK.value,
-            "container_type": ContainerType.TODAY.value,
-            "container_title": "📆 Today",
-            "batch": 1
-          },
-          "subtitle": f"Batch create Task \"{input_text}\" for Today"
-        }
+        "subtitle": f"Batch create Task \"{input_text}\" in Inbox"
       }
-    })
+    }
+  })
 
-    items.append({
-      "title": f"→ Browse...",
-      "subtitle": "Browse Areas / Projects to create Task",
-      "arg": input_text,
-      "variables": {
-        "input_text": input_text,
-        "notes": notes,
-        "action": ActionType.BROWSE.value,
+  items.append({
+    "title": f"Today",
+    "subtitle": f"Create Task \"{input_text}\" in Today",
+    "arg": input_text,
+    "variables": {
+      "input_text": input_text,
+      "notes": notes,
+      "action": ActionType.ADD.value,
+      "item_type": ItemType.TASK.value,
+      "container_type": ContainerType.TODAY.value,
+      "container_title": "📆 Today",
+    },
+    "icon": {"path": f"icons/{theme}/today.png"},
+    "valid": bool(input_text),
+    "mods": {
+      "option": {
+        "valid": True,
+        "arg": input_text,
+        "variables": {
+          "input_text": input_text,
+          "notes": notes,
+          "action": ActionType.ADD.value,
+          "item_type": ItemType.PROJECT.value,
+          "container_type": ContainerType.TODAY.value,
+          "container_title": "📆 Today",
+        },
+        "subtitle": f"Create Project \"{input_text}\" for Today"
       },
-      "icon": {"path": f"icons/{theme}/list.png"},
-      "valid": True
-    })
-  else:
-    items.append({
-      "title": "Thingsfred",
-      "subtitle": "Type ++name to create a new item",
-      "icon": {"path": f"icons/{theme}/icon.png"},
-      "valid": False
-    })
+      "cmd": {
+        "valid": bool(input_text),
+        "arg": input_text,
+        "variables": {
+          "input_text": input_text,
+          "notes": notes,
+          "action": ActionType.ADD.value,
+          "item_type": ItemType.TASK.value,
+          "container_type": ContainerType.TODAY.value,
+          "container_title": "📆 Today",
+          "batch": 1
+        },
+        "subtitle": f"Batch create Task \"{input_text}\" for Today"
+      }
+    }
+  })
+
+  items.append({
+    "title": f"→ Browse...",
+    "subtitle": "Browse Areas / Projects to create Task",
+    "arg": input_text,
+    "variables": {
+      "input_text": input_text,
+      "notes": notes,
+      "action": ActionType.BROWSE.value,
+    },
+    "icon": {"path": f"icons/{theme}/list.png"},
+    "valid": True
+  })
+# else:
+  items.append({
+    "title": "Thingsfred",
+    "subtitle": "Type ++name to create a new item",
+    "icon": {"path": f"icons/{theme}/icon.png"},
+    "valid": False
+  })
 
   print(json.dumps({"items": items}, ensure_ascii=False))
 
